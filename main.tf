e2e1968912261db36c5e5d307587268bbad73136
Paolo Oliveira <paolocmo@gmail.com>
2019-07-25 23:48:59 -0300

    remove waf

--- main.tf
+++ main.tf
@@ -10,12 +10,19 @@ module "registry" {
   environment             = "${var.environment}"
 }
 
+module "network" {
+  source                  = "./modules/network"
+  environment             = "${var.environment}"
+  vpc_cidr                = "${var.vpc_cidr}"
+  region                  = "${var.region}"
+  az_count                = "${var.az_count}"
+}
+
 module "fargate" {
   source                  = "./modules/fargate"
   environment             = "${var.environment}"
   app_name                = "${var.app_name}"
   app_port                = "${var.app_port}"
-  vpc_cidr                = "${var.vpc_cidr}"
   app_image               = "${module.registry.repository_uri}:latest"
   scale_min               = "${var.scale_min}"
   scale_max               = "${var.scale_max}"
@@ -25,10 +32,12 @@ module "fargate" {
   fargate_memory          = "${var.fargate_memory}"
   use_ssl                 = "${var.use_ssl}"
   app_count               = "${var.app_count}"
-  az_count                = "${var.az_count}"
   deploy_min_t            = "${var.deploy_min_t}"
   deploy_max_t            = "${var.deploy_max_t}"
   health_check_path       = "${var.health_check_path}"
+  vpc_id                  = "${module.network.vpc_id}"
+  public_subnet           = "${module.network.subnet_pub}"
+  private_subnet          = "${module.network.subnet_prv}"
 }
 
 module "buildndeploy" {
@@ -41,7 +50,7 @@ module "buildndeploy" {
   repository_branch       = "${var.repository_branch}"
   ecs_cluster_name        = "${module.fargate.cluster_name}"
   ecs_service_name        = "${module.fargate.service_name}"
-  task_subnet_id          = "${module.fargate.task_subnet_id}"
+  task_subnet_id          = "${module.network.subnet_prv}"
   task_secgrp_id          = "${module.fargate.task_secgrp_id}"
   github_token            = "${var.github_token}"
 }
@@ -53,15 +62,4 @@ module "hostname" {
   base_domain             = "${var.base_domain}"
 }
 
-module "protection" {
-  source                  = "./modules/waf"
-  app_name                = "${var.app_name}"
-  environment             = "${var.environment}"
-  alb_arn                 = "${module.fargate.alb_arn}"
-  protected_endpoint      = "${var.protected_endpoint}"
-  admin_remote_ipset      = "${var.admin_remote_ipset}"
-}
-
-output "app_hn" {
-  value = "${module.hostname.name}"
-}
+output "app_hn" { value = "${module.hostname.name}" }
